5ee583f4a169f1e293b5a73f20700ea915c232cb
github username <github email address>
2021-03-07 18:49:50 -0500

    Updated routes.

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,12 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
 
-  resources :comments
-  resources :users do
-    projects, only: [:new, :create, :index]
-  end
   resources :projects do 
     resources :comments
+  end
+  resources :comments
+  resources :users do
+    projects
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
